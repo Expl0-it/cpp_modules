87f3ac9d1c14e4f558d5d85e07d2d856f9cef8b0
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-14 19:34:30 +0200

    added header for Contact

---
+++ m00/ex01/Contact.hpp
@@ -0,0 +1,12 @@
+#pragma
+#include <iostream>
+
+class Contact {
+	public:
+		int			id;
+		std::string	firstName;
+		std::string	lastName;
+		std::string	nickname;
+		std::string	number;
+		std::string	darksetSecret;
+};
