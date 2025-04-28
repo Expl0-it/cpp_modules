cbd05fd615f2a914512001464bfe09a08d3a81e0
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-28 18:49:16 +0200

    added weapon calss

---
+++ m01/ex03/Weapon.cpp
@@ -0,0 +1,19 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon() : _type("Default Weapon"){
+	std::cout << "Default weapon created" << std::endl;
+}
+
+Weapon::Weapon(std::string type) : _type(type){
+	std::cout << "Weapon of type: " << _type << " created" << std::endl;
+}
+Weapon::~Weapon(){
+	std::cout << "Weapon: " << _type <<  "destroyed" << std::endl;
+}
+
+std::string const	Weapon::getType(void) const{
+	return (this->_type);
+}
+void	Weapon::setType(std::string newType){
+	this->_type = newType;
+}
