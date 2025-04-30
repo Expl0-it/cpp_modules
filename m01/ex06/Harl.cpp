7603550f7857487e069959e2dce35550b24ae222
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-30 21:15:49 +0200

    added Harl (using switch and enum)

---
+++ m01/ex06/Harl.cpp
@@ -0,0 +1,66 @@
+#include "Harl.hpp"
+
+Harl::Harl() {
+	std::cout << "Harl created" << std::endl;
+}
+
+Harl::~Harl() {
+	std::cout << "Harl destroyed" << std::endl;
+}
+
+void	Harl::complain(std::string level){
+	switch (assignEnum(level)) {
+		case hu::DEBUG:
+			this->debug();
+			__attribute__ ((fallthrough));
+		case hu::INFO:
+			this->info();
+			__attribute__ ((fallthrough));
+		case hu::WARNING:
+			this->warning();
+			__attribute__ ((fallthrough));
+		case hu::ERROR:
+			this->error();
+			break ;
+		default:
+			std::cout << "Unknown level" << std::endl;
+			break;
+	}
+}
+
+hu::t_level	Harl::assignEnum(std::string level){
+	if (level == "DEBUG")
+		return (hu::DEBUG);
+	if (level == "INFO")
+		return (hu::INFO);
+	if (level == "WARNING")
+		return (hu::WARNING);
+	if (level == "ERROR")
+		return (hu::ERROR);
+	return ((hu::t_level)-1);
+}
+
+void	Harl::debug(void){
+	std::cout <<
+	"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+	<< std::endl;
+}
+
+void	Harl::info(void){
+	std::cout <<
+	"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	<< std::endl;
+
+}
+
+void	Harl::warning(void){
+	std::cout <<
+	"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+	<< std::endl;
+}
+
+void	Harl::error(void){
+	std::cout <<
+	"This is unacceptable! I want to speak to the manager now."
+	<< std::endl;
+}
---
+++ m01/ex06/Harl.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+namespace harl_utils {
+	typedef enum e_level{
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR,
+}				t_level;
+};
+
+namespace hu = harl_utils;
+
+class Harl {
+
+public:
+	Harl();
+	~Harl();
+
+	void	complain(std::string level);
+
+private:
+	hu::t_level	assignEnum(std::string level);
+	void		debug(void);
+	void		info(void);
+	void		warning(void);
+	void		error(void);
+};
