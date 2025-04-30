f4ebe5f1b52cdc62eb214d07bb2649dd716ca4d7
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-04-30 19:07:46 +0200

    added harl header

---
+++ m01/ex05/Harl.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+
+class Harl {
+
+public:
+	Harl();
+	~Harl();
+
+	void	complain(std::string level);
+
+private:
+	std::map<std::string, void (Harl::*)(void)>	levels;
+
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+};
