3f47c9d3e18247fb1c8bafcd2b52985bb572493e
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-03 18:21:56 +0200

    changed main for m02ex00

--- m02/ex00/main.cpp
+++ m02/ex00/main.cpp
@@ -4,10 +4,8 @@
 
 int main( void ) {
 Fixed a;
-a.setRawBits(420);
 Fixed b( a );
 Fixed c;
-c.setRawBits(2137);
 c = b;
 std::cout << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
