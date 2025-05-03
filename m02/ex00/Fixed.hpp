8694d667da623044e7c0abbe72918869a6c0e682
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-03 18:19:45 +0200

    added Fixed class

---
+++ m02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed {
+public:
+	Fixed();
+	Fixed(const Fixed &copy);
+	Fixed &operator=(const Fixed &assign);
+	~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+private:
+	int					_value;
+	static const int	_fractionalBits = 8;
+};
