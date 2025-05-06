741edeed4fb75deff6c76b0177addc16f7eb8320
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-06 16:57:26 +0200

    added test main

---
+++ m02/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include "Fixed.hpp"
+#include <iostream>
+int main( void ) {
+
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+
+std::cout << b << std::endl;
+
+std::cout << Fixed::max( a, b ) << std::endl;
+
+Fixed c;
+
+std::cout << b / c << std::endl;
+
+return 0;
+}
