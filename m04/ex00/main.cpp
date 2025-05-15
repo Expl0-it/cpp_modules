4dc3e57395c01a0bf5644a7c9b937fdc675d9f75
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-15 14:12:07 +0200

    m04ex00 added some more test to check types of different calsses

--- m04/ex00/main.cpp
+++ m04/ex00/main.cpp
@@ -7,19 +7,25 @@
 int main()
 {
 	{
-    Animal  *animal = new Animal();
-    Animal  *cat = new Cat();
-    Animal  *dog = new Dog();
+	    Animal  *animal = new Animal();
+	    Animal  *cat = new Cat();
+	    Animal  *dog = new Dog();
 
-    animal->makeSound();
-    cat->makeSound();
-    dog->makeSound();
+    	animal->makeSound();
+    	cat->makeSound();
+    	dog->makeSound();
 
-	cat->Animal::makeSound();
+		cat->Animal::makeSound();
 
-    delete animal;
-    delete cat;
-    delete dog;
+		std::cout << "Animal type: " << animal->getType() << std::endl;
+		std::cout << "Dog type: " << dog->getType() << std::endl;
+		std::cout << "Cat type: " << cat->getType() << std::endl;
+
+	
+
+	    delete animal;
+    	delete cat;
+    	delete dog;
 	}
 
 	{
@@ -28,6 +34,10 @@ int main()
 
 		wronganimal->makeSound();
 		wrongcat->makeSound();
+
+		std::cout << "WrongAnimal type: " << wronganimal->getType() << std::endl;
+		std::cout << "WrongCat type: " << wrongcat->getType() << std::endl;
+		
 
 		delete wronganimal;
 		delete wrongcat;
