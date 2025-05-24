b38319638eeef2955d7bffc2a30739a38ff24cba
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-24 10:45:26 +0200

    added Form

---
+++ m05/ex01/Form.cpp
@@ -0,0 +1,82 @@
+#include "Form.hpp"
+
+Form::Form(std::string name, int signGrade, int executeGrade)
+: _name(name), _signed(false), _signGrade(signGrade), _execGrade(executeGrade)
+{
+	if (signGrade < 1 || executeGrade < 1)
+		throw GradeTooHighException();
+	if (signGrade > 150 || executeGrade > 150)
+		throw GradeTooLowException();
+	std::cout << "Form " << _name << " created with signGrade" << _signGrade << "and execGrade" << executeGrade << std::endl;
+}
+
+Form::Form(const Form& src)
+: _name(src._name), _signed(src._signed), _signGrade(src._signGrade), _execGrade(src._execGrade)
+{
+	std::cout << "Form copy constructor called" << std::endl;
+}
+
+Form&			Form::operator=(const Form& src)
+{
+	if (this != &src) {
+		_signed = src._signed;
+		_signGrade = src._signGrade;
+		_execGrade = src._execGrade;
+	}
+	std::cout << "Form assigment constructor called" << std::endl;
+	return (*this);
+}
+
+std::ostream&	operator<<(std::ostream& stream, const Form& form)
+{
+	stream << 
+		"Form " << form.getName() <<
+		" has signGrade: " <<
+		form.getSignGrade() <<
+		"execGrade " <<
+		form.getExecuteGrade() <<
+		"and its signed status is: " <<
+		form.getSigned() <<
+		std::endl;
+	return (stream);
+}
+
+Form::~Form()
+{
+	std::cout << "Form desturctor called" << std::endl;
+}
+
+const std::string	Form::getName() const
+{
+	return (_name);
+}
+
+bool				Form::getSigned() const
+{
+	return (_signed);
+}
+
+int					Form::getSignGrade() const
+{
+	return (_signGrade);
+}
+
+int					Form::getExecuteGrade() const
+{
+	return (_execGrade);
+}
+
+void				Form::beSigned(Bureaucrat& b)
+{
+	if (b.getGrade() > this->_signGrade)
+		throw Form::GradeTooLowException();
+	_signed = true;
+}
+
+const char*			Form::GradeTooHighException::what() const throw() {
+	return ("Form: Grade too high exception");
+}
+
+const char*			Form::GradeTooLowException::what() const throw() {
+	return ("Form: Grade too low exception");
+}
---
+++ m05/ex01/Form.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+class Form {
+public:
+	Form(std::string name, int signGrade, int execGrade);
+	Form(const Form& src);
+	Form& operator=(const Form& src);
+	~Form();
+
+	const std::string	getName() const;
+	bool				getSigned() const;
+	int					getSignGrade() const;
+	int					getExecuteGrade() const;
+	void				beSigned(Bureaucrat& b);
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char* what() const throw();
+	};
+
+private:
+	Form();
+	const std::string	_name;
+	bool				_signed;
+	int					_signGrade;
+	int					_execGrade;
+};
+std::ostream&	operator<<(std::ostream& stream, const Form& form);
