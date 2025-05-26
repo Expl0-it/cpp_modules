116100b21ba7b4f97ce7b28e92aa9ae3332a7551
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-26 09:54:13 +0200

    added main for tests

--- m05/ex02/main.cpp
+++ m05/ex02/main.cpp
@@ -1,29 +1,40 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main (void) {
 	Bureaucrat low("Jake", 150);
 	Bureaucrat high("Bubblegum", 1);
-	Bureaucrat mid("Finn", 75);
+	Bureaucrat mid("Finn", 42);
 
 	ShrubberyCreationForm easy("file");
-	ShrubberyCreationForm medium("file");
-	ShrubberyCreationForm hard("file");
+	RobotomyRequestForm medium("human");
+	PresidentialPardonForm hard("citizen");
 	try {
 		ShrubberyCreationForm rand("file");
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
 
+	high.executeForm(easy);
 	low.signForm(easy);
+	low.signForm(medium);
 	low.signForm(hard);
 	mid.signForm(easy);
 	mid.signForm(medium);
 	mid.signForm(hard);
-	high.signForm(easy);
+	high.signForm(medium);
+	high.signForm(hard);
 	high.signForm(hard);
 	low.executeForm(easy);
+	low.executeForm(medium);
+	low.executeForm(hard);
+	mid.executeForm(easy);
+	mid.executeForm(medium);
+	mid.executeForm(hard);
+	high.executeForm(medium);
 	high.executeForm(hard);
 
 	return 0;
