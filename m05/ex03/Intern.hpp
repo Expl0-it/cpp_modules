279bdb78956dabd162d84f58553ac3bced9a57e1
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-05-27 08:25:41 +0200

    m05ex03 added header for intern

---
+++ m05/ex03/Intern.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+class Intern {
+public:
+	Intern();
+	Intern(const Intern &src);
+	Intern &operator=(const Intern &src);
+	~Intern();
+
+	AForm*	makeForm(std::string formName, std::string formTarget) const;
+
+private:
+	AForm*	_makeShrubberyCreationForm(std::string formTarget) const;
+	AForm*	_makeRobotomyRequestForm(std::string formTarget) const;
+	AForm*	_makePresidentialPardonForm(std::string formTarget) const;
+};
