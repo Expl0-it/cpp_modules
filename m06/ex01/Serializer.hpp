c85e23570192dfdc92356ec1643105a7cb3df305
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-03 13:17:01 +0200

    m06ex01 added serializer.h

---
+++ m06/ex01/Serializer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+#include "Data.hpp"
+
+class  Serializer {
+public:
+	static uintptr_t	serialize(Data* ptr);
+	static Data* 		deserialize(uintptr_t raw);
+private:
+	Serializer();
+	Serializer(const  Serializer &src);
+	Serializer &operator=(const  Serializer &src);
+	~Serializer();
+};
