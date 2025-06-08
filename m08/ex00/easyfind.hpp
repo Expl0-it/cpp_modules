f8751d53c4dde702e4a41a991e0b898cfe187750
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-08 14:11:12 +0200

    m08ex00 added easyfind fn template

---
+++ m08/ex00/easyfind.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+
+template<typename T>
+typename T::iterator	easyfind(T& haystack, int needle) {
+	return (std::find(haystack.begin(), haystack.end(), needle));
+}
