9d0b18d6186fdd6c5a60be185225fb6ecc70efc2
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-08 13:16:15 +0200

    m07ex02 added Array.hpp class template

---
+++ m07/ex02/Array.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <cstring>
+
+template<typename T>
+class Array {
+public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array &src);
+	Array &operator=(const Array &src);
+	~Array();
+
+	T& operator[](unsigned int i);
+	T& operator[](unsigned int i) const;
+
+	unsigned int	size() const;
+
+	class OutOfBoundsException : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return ("Index out of bounds exception");
+		}
+	};
+
+private:
+	T*				elements;
+	unsigned int	elements_count;
+};
