290181c9723a7906dc0db17f020134cf600b6061
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-09 16:54:10 +0200

    m08ex01 updated main with more tests

--- m08/ex01/main.cpp
+++ m08/ex01/main.cpp
@@ -1,6 +1,13 @@
 #include "Span.hpp"
 
+#include <cstdlib>
+#include <ctime>
+
 int main(void) {
+
+	srand(time(NULL));
+
+	std::cout << "	Subject test:" << std::endl;
 	{
 		Span sp = Span(5);
 		sp.addNumber(6);
@@ -10,6 +17,27 @@ int main(void) {
 		sp.addNumber(11);
 		std::cout << sp.shortestSpan() << std::endl;
 		std::cout << sp.longestSpan() << std::endl;
+	}
+	std::cout << std::endl << "	100 test:" << std::endl;
+	{
+		Span span(100);
+		std::vector<int> tmp;
+		for (int i = 0; i < 100; i++)
+			tmp.push_back(rand() % 1000000);
+		span.addNumbers(tmp.begin(), tmp.end());
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
+	}
+
+	std::cout << std::endl << "	100,000 test:" << std::endl;
+	{
+		Span span(100000);
+		std::vector<int> tmp;
+		for (int i = 0; i < 10000; i++)
+			tmp.push_back(rand() % 100000000);
+		span.addNumbers(tmp.begin(), tmp.end());
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
 	}
 	return (0);
 }
