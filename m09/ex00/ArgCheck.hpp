934a87dcc8542c928925d81ec3db85631a598185
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-12 16:48:08 +0200

    m09ex00 added argcheck

---
+++ m09/ex00/ArgCheck.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef ARGCHECK_HPP
+# define ARGCHECK_HPP
+
+# include <iostream>
+
+class	ArgCheck{
+private:
+	ArgCheck();
+	ArgCheck(const ArgCheck &src);
+	ArgCheck &operator=(const ArgCheck &src);
+	~ArgCheck();
+
+public:
+	static bool	checkArgCount(int argc, int expected);
+};
+
+#endif // !ARGCHECK_HPP
