db44096fef1f69b8460a70d675931ea2ad8a1b57
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-06-15 13:14:24 +0200

    all previous ArgCheck impl now print to cerr insted of cout

--- m01/ex06/ArgCheck.cpp
+++ m01/ex06/ArgCheck.cpp
@@ -3,7 +3,7 @@
 bool	ArgCheck::checkArgCount(int argc, int expected){
 	if (argc != expected)
 	{
-		std::cout << "Error: Wrong argument count" << std::endl;
+		std::cerr << "Error: Wrong argument count" << std::endl;
 		return (false);
 	}
 	return (true);
